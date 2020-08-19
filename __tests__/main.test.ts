de11c6c1ae758e6daa6d1002e2263a24bb2ce181
philschatz <253202+philschatz@users.noreply.github.com>
2020-08-19 12:21:11 -0500

    initial

---
+++ __tests__/main.test.ts
@@ -0,0 +1,10 @@
+describe('example test suite', () => {
+  // Read more about fake timers
+  // http://facebook.github.io/jest/docs/en/timer-mocks.html#content
+  jest.useFakeTimers()
+
+  it('passes a trivial test', () => {
+    expect(true).toBe(true)
+  })
+
+})
